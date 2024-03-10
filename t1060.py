ca9903f109ab551c78feb20ba4ccf87d4d653cb0
fish_super <114349415+fishsuper@users.noreply.github.com>
2024-03-10 22:43:38 +0800

    Add files via upload

---
+++ t1060.py
@@ -0,0 +1,55 @@
+
+def KthMissingNumber(A, K):
+    headNumber = A[0]
+    tempNumber = headNumber
+    k = 0
+    index = 0
+    while k < K:
+        tempNumber += 1
+        if A[index+1] != tempNumber:
+            k += 1
+        else:
+            index += 1
+        
+    return tempNumber
+
+A = [4, 7, 9, 11, 15, 20]
+K = 10
+
+print('The Kth missing number in array A:', KthMissingNumber(A, K))
+
+def KthMissing(A,K):
+    n = len(A)
+    global index
+    for i in range(n):
+        if A[i]-A[0]-i > K:
+            index = i
+            break
+    temp = A[index-1]    
+    k = A[index-1]-A[0]-(index-1)
+    while k < K:
+        temp += 1
+        k += 1
+    return temp
+
+'''
+    super_index = len(A)-1
+    lower_index = 0
+    index = (super_index + lower_index)//2
+
+    while B[index] < K:
+        if (A[index]-A[0])-index < K:
+            lower_index = index
+            index = (super_index + lower_index)//2
+        elif (A[index]-A[0])-index > K:
+            super_index = index
+            index = (super_index + lower_index)//2
+    
+    return A[index]
+    
+'''
+    
+print('The Kth missing number', KthMissing(A,K))
+
+        
+
