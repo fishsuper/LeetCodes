874fd3a6651fcf6d5403f2a554fd1199ac79ec6f
fish_super <114349415+fishsuper@users.noreply.github.com>
2024-05-11 14:36:50 +0800

    Add files via upload

---
+++ t112.py
@@ -0,0 +1,42 @@
+# 路径总和
+
+# 递归
+# Definition for a binary tree node.
+# class TreeNode:
+#    def __init__(self, val=0, left=None, right=None):
+#        self.val = val
+#        self.left = left
+#        self.right = right
+class Solution:
+    def hasPathSum(self, root, targetSum: int) -> bool:
+        if not root:
+            return False
+        if not root.left and not root.right:
+            return targetSum == root.val
+        return self.hasPathSum(root.left,targetSum-root.val) or self.hasPathSum(root.right,targetSum - root.val)
+
+
+# 广度优先搜索 BFS
+import collections
+class Solution:
+    def hasPathSum(self, root, sum: int) -> bool:
+        if not root:
+            return False
+        que_node = collections.deque([root])
+        que_val = collections.deque([root.val])
+        while que_node:
+            now = que_node.popleft()
+            temp = que_val.popleft()
+            if not now.left and not now.right:
+                if temp == sum:
+                    return True
+                continue
+            if now.left:
+                que_node.append(now.left)
+                que_val.append(now.left.val + temp)
+            if now.right:
+                que_node.append(now.right)
+                que_val.append(now.right.val + temp)
+        return False
+
+
